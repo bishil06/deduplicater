a7f4db1dad47d3b4d233e3f72be3fcb5fb3eb109
bishil06 <bishil06@naver.com>
2021-04-30 14:44:10 +0900

    Fix: 필요없는 파일 삭제
    getStat.js 와 테스트 삭제

--- src/getStat.js
+++
@@ -1,7 +0,0 @@
-import isReadableFile from './isReadableFile.js';
-import { promises as fsPromises } from 'fs';
-
-export default function getState(filePath) {
-    return isReadableFile(filePath)
-        .then((r) => r ? fsPromises.stat(filePath) : r);
-}
