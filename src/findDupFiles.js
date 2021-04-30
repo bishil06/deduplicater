2fad3a073a6ede0b0bca13b6318901141b421416
bishil06 <bishil06@naver.com>
2021-05-01 08:47:21 +0900

    Feat: findDupFiles hash값 로직 추가
    map 에 hash값을 키로 파일을 저장함

--- src/findDupFiles.js
+++ src/findDupFiles.js
@@ -2,16 +2,26 @@ import isSameFile from './isSameFile.js';
 
 export default async function findDupFiles(fInfoList) {
     const len = fInfoList.length;
-    const result = new Set();
+    const dupFileMap = new Map();
+    // const result = new Set();
 
     for(let i=0; i<len; i++) {
         for(let j=i+1; j<len; j++) {
             if (await isSameFile(fInfoList[i], fInfoList[j])) {
-                result.add(fInfoList[i]);
-                result.add(fInfoList[j]);
+                const dupFileSet = dupFileMap.get(fInfoList[i].calcFinish);
+                if (dupFileSet === undefined) {
+                    dupFileMap.set(fInfoList[i].calcFinish, new Set([fInfoList[i], fInfoList[j]]));
+                }
+                else {
+                    // dupFileList.push(fInfoList[i]);
+                    dupFileSet.add(fInfoList[j]);
+                    dupFileMap.set(fInfoList[i].calcFinish, dupFileSet);
+                }
+                // result.add(fInfoList[i]);
+                // result.add(fInfoList[j]);
             }
         }
     }
 
-    return result;
+    return dupFileMap;
 }
