2a741fa853bb218f3d34d07300c8f49038c9018b
bishil06 <bishil06@naver.com>
2021-05-01 08:48:03 +0900

    Docs: isReadableFile 주석 추가
    추후 추가 에러 대응 코드 추가할 예정

--- src/isReadableFile.js
+++ src/isReadableFile.js
@@ -2,9 +2,9 @@ import { promises as fsPromises, constants } from 'fs';
 
 export default function isReadableFile(filePath) {
     return fsPromises.access(filePath, constants.R_OK)
-        .then(() => true)
+        .then(() => true) // 접근가능
         .catch(err => {
-            if (err.code === 'EACCES') return false;
-            else console.log(err);
+            if (err.code === 'EACCES') return false; // 접근권한이 없음
+            else console.error(`access() error ${filePath} ${err}`); // 다른 에러
     });
 }
