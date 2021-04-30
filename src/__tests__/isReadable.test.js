453b3024fcaddb1579e5dc983b635d46095bae1f
bishil06 <bishil06@naver.com>
2021-04-30 14:48:24 +0900

    Fix: 잘못된 테스트 삭제
    읽기전용 파일을 생성한뒤 테스트 하도록 수정할 예정

--- src/__tests__/isReadable.test.js
+++ src/__tests__/isReadable.test.js
@@ -1,12 +1,4 @@
 import isReadableFile from '../isReadableFile.js';
 
 describe('isReadableFile', () => {
-    test('isReadableFile readable file',async () => {
-        await isReadableFile('readableFile').then((r) => expect(r).toBe(true));
-    })
-
-    test('isReadableFile not readable file', async () => {
-        // notReadableFile permision --wx-wx-wx
-        await isReadableFile('notReadableFile').then((r) => expect(r).toBe(false));
-    })
 });
