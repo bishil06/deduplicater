cf5539dec15f9f1afde0fa044e36211a83dea8c1
bishil06 <bishil06@naver.com>
2021-05-03 11:46:37 +0900

    Fix: isReadable 결과 수정
    promise의 결과로 undefined 가 나와야 true를 반환함

--- src/isReadableFile.js
+++ src/isReadableFile.js
@@ -1,10 +1,13 @@
 import { promises as fsPromises, constants } from 'fs';
 
 export default function isReadableFile(filePath) {
-    return fsPromises.access(filePath, constants.R_OK)
-        .then(() => true) // 접근가능
+    const result = fsPromises.access(filePath, constants.F_OK | constants.R_OK)
         .catch(err => {
             if (err.code === 'EACCES') return false; // 접근권한이 없음
             else console.error(`access() error ${filePath} ${err}`); // 다른 에러
-    });
+            return false;
+        })
+        .then((r) => (r === undefined) ? true : false) // 접근가능
+        
+    return result;
 }
